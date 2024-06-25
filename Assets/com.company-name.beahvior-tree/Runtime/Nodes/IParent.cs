c6f1550ac2b96857de47f7d13c9ec88e0a3b484b
so-t <sophthurmond@gmail.com>
2024-06-25 10:47:58 -0700

    Removed duplicate directory

--- Assets/com.company-name.beahvior-tree/Runtime/Nodes/IParent.cs
+++
@@ -1,7 +0,0 @@
-namespace BehaviorTree.Nodes
-{
-    public interface IParent : INode
-    {
-        void AddChild(INode child);
-    }
-}
--- Assets/com.company-name.beahvior-tree/Runtime/Status.cs
+++
@@ -1,9 +0,0 @@
-namespace BehaviorTree
-{
-    public enum Status
-    {
-        Success,
-        Failure,
-        Running
-    }
-}
